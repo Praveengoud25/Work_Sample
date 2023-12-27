2ea8006af665ca14c04e8fb237af4d3a7dc33371
Praveengoud25 <154995562+Praveengoud25@users.noreply.github.com>
2023-12-27 20:23:52 +0530

    Add files via upload

---
+++ hist mig/copyscript.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+year=$1
+month=$2
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/defcbac5-0158-411c-b45b-4251e9cd2e26/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/defcbac5-0158-411c-b45b-4251e9cd2e26/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/dc3c0cdc-c086-47ba-a8c9-eb18cd2933ea/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/dc3c0cdc-c086-47ba-a8c9-eb18cd2933ea/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/c06ff0f8-1c79-4b25-97dd-c3e95e8e4d11/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/c06ff0f8-1c79-4b25-97dd-c3e95e8e4d11/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/25a5a333-ced9-4f78-b55c-c0ce9608adc1/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/25a5a333-ced9-4f78-b55c-c0ce9608adc1/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/b8d3cf5d-3c3f-4407-b27b-f30e3f747514/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/b8d3cf5d-3c3f-4407-b27b-f30e3f747514/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/a0cbbf5c-f703-4fb2-a3f8-8bfef6bf8e0f/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/a0cbbf5c-f703-4fb2-a3f8-8bfef6bf8e0f/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/b450eb9c-dc17-4d2c-b5c5-3b19cf75b3b4/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/b450eb9c-dc17-4d2c-b5c5-3b19cf75b3b4/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/e6a8d557-0f7f-47a3-875d-f9908c413631/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/e6a8d557-0f7f-47a3-875d-f9908c413631/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/4ba6fec8-dd5e-419e-97fc-50852472563c/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/4ba6fec8-dd5e-419e-97fc-50852472563c/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/60969ca3-75a3-46ab-8ff9-62d6ac604b09/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/60969ca3-75a3-46ab-8ff9-62d6ac604b09/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/7e8db425-6548-4d34-adf8-d3fb2f7295b4/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/7e8db425-6548-4d34-adf8-d3fb2f7295b4/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/7fdb2dec-31d3-487b-b765-c61c3a11cd2d/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/7fdb2dec-31d3-487b-b765-c61c3a11cd2d/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/88dcf466-38b4-4994-821e-89c0da68c3f4/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/88dcf466-38b4-4994-821e-89c0da68c3f4/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/9205f129-a85f-4dde-9889-95da9d70c459/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/9205f129-a85f-4dde-9889-95da9d70c459/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/c43d153c-d6e9-4fa5-8f79-fd5a846250fe/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/c43d153c-d6e9-4fa5-8f79-fd5a846250fe/$year/$month/
+s3-dist-cp --src=s3://dev-pd-inbound/PHHD/PWP_Archives/e52af41f-8526-4e9c-be6e-c7b9b4d14804/$year/$month/ --dest=hdfs:///PHHD/PWP_Archives/e52af41f-8526-4e9c-be6e-c7b9b4d14804/$year/$month/
